b207b5134315f8476f469fc8591dec202f29931e
Kandresdiaz <kevina0416@gmail.com>
2026-03-27 07:20:53 -0500

    fix: Resilience in shopify setup for missing HOST

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -6,7 +6,7 @@ export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY || "",
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   scopes: ["read_products", "read_orders", "read_all_orders", "write_products"],
-  hostName: process.env.HOST?.replace(/https?:\/\//, "") || "",
+  hostName: (process.env.HOST || process.env.VERCEL_URL || "conciliai.com").replace(/https?:\/\//, ""),
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: true,
   logger: {
